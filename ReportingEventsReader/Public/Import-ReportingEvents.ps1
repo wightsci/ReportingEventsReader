b69e335c1def75401e27a4dd6460d23a03331ba0
Stuart Squibb <stuart_squibb@hotmail.co.uk>
2020-04-13 19:52:02 +0100

    Added Import-ReportingEvents.ps1

---
+++ ReportingEventsReader/Public/Import-ReportingEvents.ps1
@@ -0,0 +1,41 @@
+[CmdletBinding()]
+Param(
+[String]$EventsPath="$env:Windir\SoftwareDistribution\ReportingEvents.log"    
+)
+$header = "EventInstanceID",
+"TimeAtTarget",
+"EventNamespaceID",
+"EventID",
+"EventSource",
+"UpdateID",
+"RevisionID",
+"SequenceNumber",
+"AppName",
+"Result",
+"Action",
+"Message",
+"MessageID"
+
+$ReportingEvents = Import-CSV -Path $EventsPath -Header $header -Delimiter  `t  
+
+$ReportingEvents = $ReportingEvents | 
+Select-Object *,
+@{N='EventName';E={($_.EventID -Split ' ')[1]}},
+@{N='EventNumber';E={($_.EventID -Split ' ')[0]}},
+@{N='DateTimeAtTarget';E={([DateTime]::ParseExact($_.TimeAtTarget, 'yyyy-MM-dd HH:mm:ss:fffzzz',[CultureInfo]::CurrentCulture))}}
+
+$ReportingEvents | 
+Select-Object "EventInstanceID",
+"DateTimeAtTarget",
+"EventNamespaceID",
+@{N="EventID";E={$_.EventNumber}},
+"EventName",
+"EventSource",
+"UpdateID",
+"RevisionID",
+"SequenceNumber",
+"AppName",
+"Result",
+"Action",
+"Message",
+"MessageID"
